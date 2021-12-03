004eceb3e7bbe981ef76b1d9d6f010e91447a3e2
jopark-20 <81044560+jopark-20@users.noreply.github.com>
2021-12-02 20:24:15 -0500

    Add files via upload

--- fashionforecast/ClothingCollectionViewCell.swift
+++ fashionforecast/ClothingCollectionViewCell.swift
@@ -9,28 +9,17 @@ import UIKit
 
 class ClothingCollectionViewCell: UICollectionViewCell {
     private var clothingImageView = UIImageView()
-    private var clothingLabel = UILabel()
-    private var clothingDesc = UITextView()
     
     override init(frame: CGRect){
         super.init(frame: frame)
-        contentView.layer.cornerRadius = 5
+        contentView.layer.cornerRadius = 8
+        contentView.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        contentView.layer.borderWidth = 3
         contentView.clipsToBounds = true
         
         clothingImageView.contentMode = .scaleAspectFit
         clothingImageView.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(clothingImageView)
-        
-        clothingLabel.textColor = .black
-        clothingLabel.font = .systemFont(ofSize: 20, weight: .semibold)
-        clothingLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(clothingLabel)
-        
-        clothingDesc.textColor = .lightGray
-        clothingDesc.font = .systemFont(ofSize: 15, weight: .regular)
-        clothingDesc.textAlignment = .center
-        clothingDesc.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(clothingDesc)
         
         setUpConstraints()
     }
@@ -41,24 +30,14 @@ class ClothingCollectionViewCell: UICollectionViewCell {
     
     func configure(for clothing:Clothing){
         clothingImageView.image = UIImage(named: clothing.imageName)
-        clothingLabel.text = clothing.type
-        clothingDesc.text = clothing.description
     }
     
     func setUpConstraints(){
         NSLayoutConstraint.activate([
-            clothingImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
-            clothingImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            clothingImageView.heightAnchor.constraint(equalToConstant: 100),
-            clothingImageView.widthAnchor.constraint(equalToConstant: 100)
-        ])
-        NSLayoutConstraint.activate([
-            clothingLabel.topAnchor.constraint(equalTo: clothingImageView.bottomAnchor, constant: 10),
-            clothingLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
-        ])
-        NSLayoutConstraint.activate([
-            clothingDesc.topAnchor.constraint(equalTo: clothingLabel.bottomAnchor, constant: 10),
-            clothingDesc.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+            clothingImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            clothingImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            clothingImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            clothingImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
         ])
     }
     
